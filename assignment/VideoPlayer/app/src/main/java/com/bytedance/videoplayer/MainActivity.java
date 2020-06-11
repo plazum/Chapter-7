bef451107bf92d3de43ad2e74efeacfb5a4d0228
plazum <34277374+plazum@users.noreply.github.com>
2020-06-11 20:01:06 +0800

    Homework

--- assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
+++ assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
@@ -1,19 +1,64 @@
 package com.bytedance.videoplayer;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.MediaController;
 
 import com.bumptech.glide.Glide;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+    private MyVideoView videoView;
+    private MediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = findViewById(R.id.imageView);
-        String url = "https://s3.pstatp.com/toutiao/static/img/logo.271e845.png";
-        Glide.with(this).load(url).into(imageView);
+//        ImageView imageView = findViewById(R.id.imageView);
+//        String url = "https://s3.pstatp.com/toutiao/static/img/logo.271e845.png";
+//        Glide.with(this).load(url).into(imageView);
+        videoView = findViewById(R.id.video_view);
+        mediaController = new MediaController(this);
+        mediaController.setMediaPlayer(videoView);
+        videoView.setMediaController(mediaController);
+
+        Intent intent = getIntent();
+        Uri realUri;
+        Uri data = intent.getData();
+
+        if(data != null) {
+            realUri = data;
+        } else {
+            realUri = Uri.parse("android.resource://" + getPackageName() + "/raw/" + R.raw.sample);
+        }
+
+        videoView.setVideoURI(realUri);
+        videoView.start();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            Objects.requireNonNull(getSupportActionBar()).hide();
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            Objects.requireNonNull(getSupportActionBar()).show();
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
     }
 }
---
+++ assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MyVideoView.java
@@ -0,0 +1,55 @@
+package com.bytedance.videoplayer;
+
+import android.content.Context;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.VideoView;
+
+public class MyVideoView extends VideoView {
+    public MyVideoView(Context context) {
+        super(context);
+    }
+
+    public MyVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public MyVideoView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private int mVideoWidth;
+    private int mVideoHeight;
+
+
+    @Override
+    public void setVideoURI(Uri uri) {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(this.getContext(), uri);
+        mVideoWidth = Integer.parseInt(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH));
+        mVideoHeight = Integer.parseInt(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT));
+        super.setVideoURI(uri);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = getDefaultSize(mVideoWidth, widthMeasureSpec);
+        int height = getDefaultSize(mVideoHeight, heightMeasureSpec);
+        if (mVideoWidth > 0 && mVideoHeight > 0) {
+            if (mVideoWidth * height > width * mVideoHeight) {
+                height = width * mVideoHeight / mVideoWidth;
+            } else if (mVideoWidth * height < width * mVideoHeight) {
+                width = height * mVideoWidth / mVideoHeight;
+            }
+        }
+        setMeasuredDimension(width, height);
+    }
+}
